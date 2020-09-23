bc512ca62f79a99afe04dc94c19a50d5bc84894e
JuanKmilo11 <juan.kmilo11@hotmail.com>
2020-09-23 13:57:38 -0500

    Added Regular Expressions module

--- src/app/components/home/re/re.component.ts
+++ src/app/components/home/re/re.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-re',
   templateUrl: './re.component.html',
+  styleUrls: ['./re.component.scss'],
   styles: [
   ]
 })
 export class ReComponent implements OnInit {
+
+  msg="";
+  coincide=false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form: { value: { reg: string | RegExp; intext: string; }; }) {
+    let er = new RegExp(form.value.reg);
+    this.coincide = er.test(form.value.intext);
+    if(this.coincide){
+      this.msg="Coincide";
+    }else{
+      this.msg="No coincide";
+    }
+    console.log('Test: ',er.test("abababc")," | exp: ",form.value.reg);
+  }
+
+  comp(){
+    console.log("Automata");
+  }
 }
